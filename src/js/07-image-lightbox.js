1176b94df61c4b7ecbf5550d115e3332ee8dc564
Timo Krieger <timo.krieger@maximago.de>
2021-12-14 16:51:26 +0100

    fix format

--- src/js/07-image-lightbox.js
+++ src/js/07-image-lightbox.js
@@ -40,156 +40,156 @@ when lightbox is opened for inlined SVG:
 
 ;
 (function () {
-    'use strict'
-    var lightbox
-    var config = (document.getElementById('site-script') || { dataset: {} }).dataset
-    var content
+  'use strict'
+  var lightbox
+  var config = (document.getElementById('site-script') || { dataset: {} }).dataset
+  var content
 
-    function init() {
-        if (!lightbox) {
-            lightbox = document.createElement('div')
-            lightbox.setAttribute('aria-modal', 'true')
-            lightbox.className = 'modal'
-            var closeLink = document.createElement('a')
-            // set href to make it selectable with tab / assistive technologies
-            closeLink.href = '#'
-            closeLink.className = 'close'
-            closeLink.setAttribute('title', 'Close lightbox')
-            if (config.svgAs === 'svg') {
-                var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-                svg.setAttribute('class', 'copy-icon')
-                var use = document.createElementNS('http://www.w3.org/2000/svg', 'use')
-                use.setAttribute('href', window.uiRootPath + '/img/octicons-16.svg#icon-x')
-                svg.appendChild(use)
-                closeLink.appendChild(svg)
-            } else {
-                var img = document.createElement('img')
-                img.src = window.uiRootPath + '/img/octicons-16.svg#view-x'
-                img.alt = 'close icon'
-                img.className = 'x-icon'
-                closeLink.appendChild(img)
-            }
-            lightbox.appendChild(closeLink)
-            content = document.createElement('div')
-            content.className = 'content'
-            lightbox.appendChild(content)
-            var body = document.getElementsByTagName('body')[0]
-            body.appendChild(lightbox)
-            body.addEventListener('keydown', function (e) {
-                if (e.code === 'Escape' && isOpen()) {
-                    close(e)
-                }
-            })
-
-            content.addEventListener('click', close)
-            closeLink.addEventListener('click', function (e) {
-                close(e)
-                e.preventDefault()
-            })
-            closeLink.addEventListener('keydown', function (e) {
-                if (e.code === 'Space' || e.code === 'Enter') {
-                    close(e)
-                    e.preventDefault()
-                }
-            })
+  function init() {
+    if (!lightbox) {
+      lightbox = document.createElement('div')
+      lightbox.setAttribute('aria-modal', 'true')
+      lightbox.className = 'modal'
+      var closeLink = document.createElement('a')
+      // set href to make it selectable with tab / assistive technologies
+      closeLink.href = '#'
+      closeLink.className = 'close'
+      closeLink.setAttribute('title', 'Close lightbox')
+      if (config.svgAs === 'svg') {
+        var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+        svg.setAttribute('class', 'copy-icon')
+        var use = document.createElementNS('http://www.w3.org/2000/svg', 'use')
+        use.setAttribute('href', window.uiRootPath + '/img/octicons-16.svg#icon-x')
+        svg.appendChild(use)
+        closeLink.appendChild(svg)
+      } else {
+        var img = document.createElement('img')
+        img.src = window.uiRootPath + '/img/octicons-16.svg#view-x'
+        img.alt = 'close icon'
+        img.className = 'x-icon'
+        closeLink.appendChild(img)
+      }
+      lightbox.appendChild(closeLink)
+      content = document.createElement('div')
+      content.className = 'content'
+      lightbox.appendChild(content)
+      var body = document.getElementsByTagName('body')[0]
+      body.appendChild(lightbox)
+      body.addEventListener('keydown', function (e) {
+        if (e.code === 'Escape' && isOpen()) {
+          close(e)
         }
-    }
+      })
 
-    function open() {
-        lightbox.style.display = 'block'
+      content.addEventListener('click', close)
+      closeLink.addEventListener('click', function (e) {
+        close(e)
+        e.preventDefault()
+      })
+      closeLink.addEventListener('keydown', function (e) {
+        if (e.code === 'Space' || e.code === 'Enter') {
+          close(e)
+          e.preventDefault()
+        }
+      })
     }
+  }
 
-    function isOpen() {
-        return lightbox && lightbox.style.display === 'block'
+  function open() {
+    lightbox.style.display = 'block'
+  }
+
+  function isOpen() {
+    return lightbox && lightbox.style.display === 'block'
+  }
+
+  function close(e) {
+    lightbox.style.display = 'none'
+    content.firstChild.remove()
+    // don't prevent default here, as that will allow links in SVGs to work
+  }
+
+  // depending on ratio of source vs target element, make the lightbox content 90% of height or width
+  function setImageSize(img, source, target) {
+    var ratioSource = source.offsetWidth / source.offsetHeight
+    var ratioTarget = target.offsetWidth / target.offsetHeight
+    if (ratioSource < ratioTarget) {
+      img.style.height = '90vh'
+    } else {
+      img.style.width = '90vw'
     }
+  }
 
-    function close(e) {
-        lightbox.style.display = 'none'
-        content.firstChild.remove()
-        // don't prevent default here, as that will allow links in SVGs to work
+  document.querySelectorAll('.imageblock img').forEach(function (element) {
+    if (element.parentNode.nodeName === 'A') {
+      // if parent node is an anchor, keep the anchor instead of opening lightbox
+      return
     }
-
-    // depending on ratio of source vs target element, make the lightbox content 90% of height or width
-    function setImageSize(img, source, target) {
-        var ratioSource = source.offsetWidth / source.offsetHeight
-        var ratioTarget = target.offsetWidth / target.offsetHeight
-        if (ratioSource < ratioTarget) {
-            img.style.height = '90vh'
+    element.parentNode.className += ' lightbox'
+    if (typeof element.parentNode.classList.remove === 'function') {
+      element.parentNode.addEventListener('mouseover', function (e) {
+        if (element.naturalWidth <= element.offsetWidth && element.naturalHeight <= element.offsetHeight) {
+          element.parentNode.classList.remove('lightbox')
         } else {
-            img.style.width = '90vw'
+          element.parentNode.classList.add('lightbox')
         }
+      })
     }
+    element.addEventListener('click', function (e) {
+      if (element.naturalWidth <= element.offsetWidth && element.naturalHeight <= element.offsetHeight) {
+        // don't open lightbox is already shown at 100% or more
+        return
+      }
+      init()
+      var img = document.createElement('img')
+      img.src = e.currentTarget.src
+      img.alt = e.currentTarget.alt
+      setImageSize(img, element.parentNode, content.parentNode)
+      content.appendChild(img)
+      open()
+    })
+  })
 
-    document.querySelectorAll('.imageblock img').forEach(function (element) {
-        if (element.parentNode.nodeName === 'A') {
-            // if parent node is an anchor, keep the anchor instead of opening lightbox
-            return
-        }
-        element.parentNode.className += ' lightbox'
-        if (typeof element.parentNode.classList.remove === 'function') {
-            element.parentNode.addEventListener('mouseover', function (e) {
-                if (element.naturalWidth <= element.offsetWidth && element.naturalHeight <= element.offsetHeight) {
-                    element.parentNode.classList.remove('lightbox')
-                } else {
-                    element.parentNode.classList.add('lightbox')
-                }
-            })
-        }
-        element.addEventListener('click', function (e) {
-            if (element.naturalWidth <= element.offsetWidth && element.naturalHeight <= element.offsetHeight) {
-                // don't open lightbox is already shown at 100% or more
-                return
-            }
-            init()
-            var img = document.createElement('img')
-            img.src = e.currentTarget.src
-            img.alt = e.currentTarget.alt
-            setImageSize(img, element.parentNode, content.parentNode)
-            content.appendChild(img)
-            open()
-        })
+  document.querySelectorAll('.imageblock object').forEach(function (element) {
+    if (element.parentNode.nodeName === 'A') {
+      // if parent node is an anchor, keep the anchor instead of opening lightbox
+      return
+    }
+    element.parentNode.className += ' lightbox'
+    element.parentNode.addEventListener('click', function (e) {
+      init()
+      var img = document.createElement('object')
+      img.type = element.type
+      img.data = element.data
+      open()
+      setImageSize(img, element, content.parentNode)
+      if (element.getSVGDocument() && element.getSVGDocument().querySelectorAll('a').length === 0) {
+        // if the SVG doesn't contain any links, allow user to click on image to close the image
+        img.style.pointerEvents = 'none'
+      }
+      content.appendChild(img)
+      // prevent links in SVGs to open, as this should only open the lightbox
+      e.preventDefault()
     })
-
-    document.querySelectorAll('.imageblock object').forEach(function (element) {
-        if (element.parentNode.nodeName === 'A') {
-            // if parent node is an anchor, keep the anchor instead of opening lightbox
-            return
-        }
-        element.parentNode.className += ' lightbox'
-        element.parentNode.addEventListener('click', function (e) {
-            init()
-            var img = document.createElement('object')
-            img.type = element.type
-            img.data = element.data
-            open()
-            setImageSize(img, element, content.parentNode)
-            if (element.getSVGDocument() && element.getSVGDocument().querySelectorAll('a').length === 0) {
-                // if the SVG doesn't contain any links, allow user to click on image to close the image
-                img.style.pointerEvents = 'none'
-            }
-            content.appendChild(img)
-            // prevent links in SVGs to open, as this should only open the lightbox
-            e.preventDefault()
-        })
+  })
+  document.querySelectorAll('.imageblock svg').forEach(function (element) {
+    if (element.parentNode.nodeName === 'A') {
+      // if parent node is an anchor, keep the anchor instead of opening lightbox
+      return
+    }
+    element.parentNode.className += ' lightbox'
+    element.parentNode.addEventListener('click', function (e) {
+      init()
+      var img = element.cloneNode(true)
+      open()
+      // override height/width from cloned element
+      img.style.height = 'auto'
+      img.style.width = 'auto'
+      // need to select element's parent node, as offsetWidth/offsetHeight not available on SVG
+      setImageSize(img, element.parentNode, content.parentNode)
+      content.appendChild(img)
+      // prevent links in SVGs to open, as this should only open the lightbox
+      e.preventDefault()
     })
-    document.querySelectorAll('.imageblock svg').forEach(function (element) {
-        if (element.parentNode.nodeName === 'A') {
-            // if parent node is an anchor, keep the anchor instead of opening lightbox
-            return
-        }
-        element.parentNode.className += ' lightbox'
-        element.parentNode.addEventListener('click', function (e) {
-            init()
-            var img = element.cloneNode(true)
-            open()
-            // override height/width from cloned element
-            img.style.height = 'auto'
-            img.style.width = 'auto'
-            // need to select element's parent node, as offsetWidth/offsetHeight not available on SVG
-            setImageSize(img, element.parentNode, content.parentNode)
-            content.appendChild(img)
-            // prevent links in SVGs to open, as this should only open the lightbox
-            e.preventDefault()
-        })
-    })
+  })
 })()
